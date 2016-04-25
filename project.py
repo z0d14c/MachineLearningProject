ab8159032248f04ac739b7bc5b5e51a47ddccebc
thomas grice <augustus.grice@gmail.com>
2016-04-25 02:04:08 -0500

    comments

--- project.py
+++ project.py
@@ -6,11 +6,15 @@ import numpy as np
 from sklearn.naive_bayes import GaussianNB
 import util
 import sys
+# instantiate args
 args = sys.argv
+# load training data
 (trainingdata, trainingclasses) = util.readData(args[1])
 trainingdata = np.array(trainingdata)
 trainingclasses = np.array(trainingclasses)
+# load test data (testclasses not used)
 (testdata, testclasses) = util.readData(args[2])
+
 classifier = GaussianNB()
 classifier.fit(trainingdata, trainingclasses)
 predictedVals = classifier.predict(trainingdata)
